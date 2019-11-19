7e38ab1ca399b641429d157e60ada9822a1b4289
Ming Xiang Chan <mingxiangchan@gmail.com>
2019-11-19 10:12:58 +0800

    initial project creation

---
+++ src/test/java/learning/youtube/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package learning.youtube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
